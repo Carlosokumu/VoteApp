032dc6bf7b12ce3ce545d3c98f6d8e9256321f5e
Carlos <carlosokumu254@gmail.com>
2022-11-05 00:21:22 -0700

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    // Constructor
+    constructor () public{
+        candidate = "Candidate 1";
+    }
+}
+    
